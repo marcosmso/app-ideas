32286529044bd385bc3535f2dfc6da1e87538ead
Marcos <marcosdp12@gmail.com>
2021-07-20 18:11:59 -0300

    implements timer with days: hours: minutes:seconds

--- Projects/1-Beginner/Countdown-Timer-App/main.js
+++ Projects/1-Beginner/Countdown-Timer-App/main.js
@@ -1,22 +1,49 @@
+days_counter = document.querySelector("#days-counter");
+hours_counter = document.querySelector("#hours-counter");
+minutes_counter = document.querySelector("#minutes-counter");
 seconds_counter = document.querySelector("#seconds-counter");
 button = document.querySelector("#button");
+date_input = document.querySelector(".date-input");
 
-var eventDate = new Date(2021, 6, 21); 
-var startDate;
-var diffInSeconds;
+const SECONDS_IN_MINUTE = 60;
+const SECONDS_IN_HOUR = 3600;
+const SECONDS_IN_DAY = 86400;
 
-// myVar = setInterval(()=>{console.log("hello")}, 1000);
-
-function updateRemainingTime() {
-    seconds_counter.innerHTML = diffInSeconds;
-    diffInSeconds = diffInSeconds - 1;
-}
+var eventDate; 
 
 button.addEventListener("click", function() {
-    startDate = new Date();
-    diffInSeconds = Math.floor((eventDate.getTime() - startDate.getTime()) / 1000);
-    setInterval(updateRemainingTime, 1000)
+    getEventDateInput();
+    setInterval(updateTimer, 1000)
 });
 
+function getEventDateInput(){
+    input = date_input.value; // string in format YYYY-MM-DD
+    day   = parseInt(input.substring(8, 10));
+    month = parseInt(input.substring(5, 7));
+    year  = parseInt(input.substring(0, 4));
+    eventDate = new Date(year, month-1, day); //month must be 0-indexed for Date
+}
 
+function updateTimer() {
 
+    var now = new Date().getTime()
+    var diff = (eventDate.getTime() - now) / 1000;
+
+    if (now > eventDate.getTime()){
+        days_counter.innerHTML = 'D';
+        hours_counter.innerHTML = 'O';
+        minutes_counter.innerHTML = 'N';
+        seconds_counter.innerHTML = 'E';
+    }
+
+    var days = Math.floor(diff / SECONDS_IN_DAY );
+    var hours = Math.floor( (diff % SECONDS_IN_DAY) / SECONDS_IN_HOUR);
+    var minutes = Math.floor( (diff % SECONDS_IN_HOUR) / SECONDS_IN_MINUTE);
+    var seconds = Math.floor( (diff % SECONDS_IN_MINUTE) );
+
+    days_counter.innerHTML = days;
+    hours_counter.innerHTML = hours;
+    minutes_counter.innerHTML = minutes;
+    seconds_counter.innerHTML = seconds;
+
+}
